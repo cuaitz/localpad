7c5c7f29c9b4432b2d5689de5dd21da58171c889
Augusto Fogaça <ocuait@gmail.com>
2025-08-27 13:23:29 -0300

    [FEAT] Remoção de arquivo morto

--- src/router.php
+++
@@ -1,6 +0,0 @@
-<?php
-$requested = __DIR__ . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-if (file_exists($requested) && is_file($requested)) {
-    return false;
-}
-require __DIR__ . '/index.php';
